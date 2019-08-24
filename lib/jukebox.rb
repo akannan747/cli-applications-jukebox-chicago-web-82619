2b27f863e11ffdf1ebcb39fa9a3893dc2e753117
Ashwin Kannan <ashwin.kannan@berkeley.edu>
2019-08-24 03:43:43 +0000

    Automatically backed up by Learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -11,5 +11,9 @@ puts say_hello(users_name)
 =end
 
 def help 
-  puts
+  puts "I accept the following commands:"
+  puts "- help : displays this help message"
+  puts "- list : displays a list of songs you can play"
+  puts "- play : lets you choose a song to play"
+  puts "- exit : exits this program"
 end
